e2bb1e395ee47d19ee466eba69928bb43f559483
Keel_im <kimh00335@gmail.com>
2024-07-16 02:05:46 +0900

    fix the indent

--- data/Notice.ts
+++ data/Notice.ts
@@ -1,18 +1,18 @@
 import { graphql } from 'react-relay';
 
 const NoticeQuery = graphql`
-query NoticeCollection {
+  query NoticeCollection {
     noticeCollection(first: 1) {
-        edges {
-            node {
-                nodeId
-                id
-                created_at
-                title
-                description
-                fixed
-            }
+      edges {
+        node {
+          nodeId
+          id
+          created_at
+          title
+          description
+          fixed
         }
+      }
     }
-}
-;`;
+  }
+`;
